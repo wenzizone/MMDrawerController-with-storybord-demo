989ab54ddd38160f2cc1851e14b4616a21170cbc
hanlzhao liu <hanliu@zynga.com>
2014-05-30 17:02:58 +0800

    init

---
+++ MMDrawerController-with-storybord-demo/MMDrawerController-with-storybord-demo/app/views/rootViewController.h
@@ -0,0 +1,13 @@
+//
+//  rootViewController.h
+//  MMDrawerController-with-storybord-demo
+//
+//  Created by harveyliu on 5/30/14.
+//  Copyright (c) 2014 com.wenzizone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rootViewController : UICollectionViewController
+
+@end
